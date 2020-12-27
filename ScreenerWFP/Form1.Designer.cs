d3f38be1f7053a6f4d404582a335fd0dcc9bcf71
jackmurray50 <47582604+jackmurray50@users.noreply.github.com>
2020-12-27 14:55:13 -0500

    Initial commit

---
+++ ScreenerWFP/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace ScreenerWFP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TablePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // TablePanel
+            // 
+            this.TablePanel.ColumnCount = 2;
+            this.TablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TablePanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TablePanel.Location = new System.Drawing.Point(0, 96);
+            this.TablePanel.Name = "TablePanel";
+            this.TablePanel.RowCount = 2;
+            this.TablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TablePanel.Size = new System.Drawing.Size(800, 354);
+            this.TablePanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TablePanel);
+            this.Name = "Form1";
+            this.Text = "Screener Helper";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TablePanel;
+    }
+}
+
---
+++ ScreenerWFP/ScreenerData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenerWFP
+{
+    /// <summary>
+    /// Class meant to allow retrieval of data from various CSVs. Note: Database in this case isn't a dbms, its a collection of csvs
+    /// </summary>
+    public static class ScreenerData
+    {
+        ///<summary>
+        ///Adds a new entry to the database.
+        ///</summary>
+        public static bool AddEntry()
+        {
+            return true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="EntryID"></param>
+        /// <returns></returns>
+        public static bool UpdateEntry(int EntryID)
+        {
+
+        }
+    }
+}
